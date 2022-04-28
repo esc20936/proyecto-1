ba3fb769e8254854d0cebf835b12bd8668122d89
esc20936 <esc20936@uvg.edu.gt>
2022-04-28 00:31:29 -0600

    first commit

---
+++ src/componentes/Prueba.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from "react";
+
+export default class Prueba extends React.Component {
+  render() {
+    return (
+      <h1>Hola mundo</h1>
+    );
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Prueba from "./componentes/Prueba";
+
+ReactDOM.render(<Prueba />, document.getElementById("root"));
